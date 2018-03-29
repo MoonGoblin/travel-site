c7d09b9773195a6f3c8b1150fadb6702e35e9570
David Rytell <daverytell@gmail.com>
2018-03-29 08:11:44 -0700

    Completed large hero bblock and intro content block.

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,29 @@
+var gulp = require("gulp");
+var watch = require("gulp-watch");
+//only importing in create method
+var browserSync = require("browser-sync").create();
+
+gulp.task("watch", function(){
+	browserSync.init({
+		notify: false, //turns of notification of web page update (optional)
+		server: {
+			baseDir: "app"
+		}
+	})
+	watch("./app/index.html", function(){
+		//gulp.start("html");
+		browserSync.reload();
+	});
+
+	//** means any future hypothetical folders
+	watch("./app/assets/styles/**/*.css", function(){
+		gulp.start("cssInject");
+	});
+});
+
+
+//In [] are dependencies that gulp must run first.
+gulp.task("cssInject", ["styles"], function(){
+	return gulp.src("./app/temp/styles/styles.css")
+	.pipe(browserSync.stream());
+});
