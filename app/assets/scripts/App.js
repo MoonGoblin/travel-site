78844c9783972f51cc8d0aefcc99c4ba6dd18b34
David Rytell <daverytell@gmail.com>
2018-04-15 16:30:52 -0700

    End of styling up Reveal Module by making class reusable

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,9 @@
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
+import $ from "jquery";
 
 
 var mobileMenu = new MobileMenu();
-var revealOnScroll = new RevealOnScroll();
+// var revealOnScroll = new RevealOnScroll();
+new RevealOnScroll($(".feature-item"), "85%"); //feature
+new RevealOnScroll($(".testimonial"), "60%"); //testimonials
