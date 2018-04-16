ae56de68e0224dccd43543ee52e6897f8e70c281
David Rytell <daverytell@gmail.com>
2018-04-16 15:35:28 -0700

    Commiting first part of sticky-header - part 1 - lecture 48

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,9 +1,12 @@
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import $ from "jquery";
+import StickyHeader from "./modules/StickyHeader";
 
 
 var mobileMenu = new MobileMenu();
 // var revealOnScroll = new RevealOnScroll();
 new RevealOnScroll($(".feature-item"), "85%"); //feature
 new RevealOnScroll($(".testimonial"), "60%"); //testimonials
+
+var stickyHeader = new StickyHeader();
