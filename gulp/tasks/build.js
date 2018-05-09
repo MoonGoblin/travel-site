616063faf6c8e89a737861f13aa02299bb84715d
David Rytell <daverytell@gmail.com>
2018-05-08 20:20:03 -0700

    This completes are website and workflow automation stuff

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -2,12 +2,43 @@ var gulp = require("gulp");
 var imagemin = require("gulp-imagemin");
 var del = require("del");
 var usemin = require("gulp-usemin");
+var rev = require("gulp-rev");
+var cssnano = require("gulp-cssnano");
+var uglify = require("gulp-uglify");
+var browserSync = require("browser-sync").create();
+
+gulp.task("previewDist", function() {
+	browserSync.init({
+		notify: false, //turns of notification of web page update (optional)
+		server: {
+			baseDir: "dist"
+		}
+	})
+});
+
 
 gulp.task("deleteDistFolder", function(){
 	return del("./dist");
 });
 
-gulp.task("optimizeImages", ["deleteDistFolder"], function(){
+
+gulp.task("copyGeneralFiles", ["deleteDistFolder"], function() {
+	var pathsToCopy = [
+		"./app/**/*",
+		"!./app/index.html",
+		"!./app/assets/images/**",
+		"!./app/assets/styles/**",
+		"!./app/assets/scripts/**",
+		"!./app/temp",
+		"!./app/temp/**"
+	]
+
+	return gulp.src(pathsToCopy)
+	.pipe(gulp.dest("./dist"));
+});
+
+
+gulp.task("optimizeImages", ["deleteDistFolder", "icons"], function(){
 	return gulp.src(["./app/assets/images/**/*", "!./app/assets/images/icons", "!./app/assets/images/icons/**/*"])
 	.pipe(imagemin({
 		progressive: true,
@@ -18,10 +49,13 @@ gulp.task("optimizeImages", ["deleteDistFolder"], function(){
 });
 
 
-gulp.task("usemin", ["deleteDistFolder"], function(){
+gulp.task("usemin", ["deleteDistFolder", "styles", "scripts"], function(){
 	return gulp.src("./app/index.html")
-	.pipe(usemin())
+	.pipe(usemin({
+		css: [function(){return rev()}, function() {return cssnano()}],
+		js: [function() {return rev()}, function() {return uglify()}]
+	}))
 	.pipe(gulp.dest("./dist"));
 });
 
-gulp.task("build", ["deleteDistFolder", "optimizeImages", "usemin"]);
+gulp.task("build", ["deleteDistFolder", "copyGeneralFiles", "optimizeImages", "usemin"]);
